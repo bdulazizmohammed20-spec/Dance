3d57d757f66796aa09ae5e65f85838042eb53e7b
bdulazizmohammed20-spec <bdulazizmohammed20@gmail.com>
2026-07-18 20:59:41 +0200

    Update splash_screen.dart

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,1 +1,60 @@
+import 'package:flutter/material.dart';
 
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Container(
+              width: 120,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle,
+              ),
+              child: const Center(
+                child: Text(
+                  "D",
+                  style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Dance",
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              "AI Crypto Trading",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.greenAccent,
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
