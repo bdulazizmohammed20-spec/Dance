25a0666a06f645fa6edff6029ba57d757e8c1203
bdulazizmohammed20-spec <bdulazizmohammed20@gmail.com>
2026-07-18 20:55:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DanceApp());
+}
+
+class DanceApp extends StatelessWidget {
+  const DanceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dance',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Dance AI Crypto Trading',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.greenAccent,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
