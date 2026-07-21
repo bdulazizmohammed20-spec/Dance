677dff6e38ab1a837783c8ef5d7d13536196842e
bdulazizmohammed20-spec <bdulazizmohammed20@gmail.com>
2026-07-21 19:26:46 +0200

    Update market_provider.dart

--- lib/lib/providers/auth_provider.dart lib/providers/market_provider.dart
+++ lib/lib/providers/auth_provider.dart lib/providers/market_provider.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class AuthProvider extends ChangeNotifier {
+  bool _isLoggedIn = false;
+  bool _isLoading = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+  bool get isLoading => _isLoading;
+
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    _isLoading = true;
+    notifyListeners();
+
+    // سيتم ربطه لاحقًا بالخادم و Firebase
+    await Future.delayed(const Duration(seconds: 2));
+
+    _isLoggedIn = true;
+    _isLoading = false;
+
+    notifyListeners();
+  }
+
+  void logout() {
+    _isLoggedIn = false;
+    notifyListeners();
+  }
+}
